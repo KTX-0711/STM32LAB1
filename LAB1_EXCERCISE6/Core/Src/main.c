82b3d8be78b711bc865a30aafcf59f1eeddd744c
Trinh Nguyen <trinh.nguyenktmtbk0711@hcmut.edu.vn>
2025-09-21 16:57:39 +0700

    ex6 + ex7 + ex8 + ex9 + ex10 init

---
+++ LAB1_EXCERCISE6/Core/Src/main.c
@@ -0,0 +1,289 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+
+  uint16_t 	LEDPIN[12] = {LED12_Pin, LED1_Pin,LED2_Pin,LED3_Pin,LED4_Pin ,LED5_Pin,LED6_Pin,LED7_Pin,LED8_Pin
+          ,LED9_Pin,LED10_Pin,LED11_Pin};
+
+  void TURN_ON_ALL(){
+	  for (int j=0; j<12; ++j){
+		  HAL_GPIO_WritePin(GPIOA, LEDPIN[j], GPIO_PIN_RESET);
+	  }
+  };
+  void TURN_OFF_ALL(){
+	  for (int j=0; j<12; ++j){
+		  HAL_GPIO_WritePin(GPIOA, LEDPIN[j], GPIO_PIN_SET);
+	  }
+  };
+  void LED_ON(int i){
+	  HAL_GPIO_WritePin(GPIOA, LEDPIN[i], GPIO_PIN_RESET);
+  }
+
+  // EXCERCISE 6
+  int counter_for_test = 0;
+  void test_LED(int i){
+	  TURN_OFF_ALL();
+	  if (i>=12) i-=12;
+	  LED_ON(i);
+  }
+
+  // EXCERCISE 7
+  void clearAllclock(){
+	  for (int j=0; j<12; ++j){
+		  HAL_GPIO_WritePin(GPIOA, LEDPIN[j], GPIO_PIN_SET);
+	  }
+  }
+  // EXCERCISE 8
+  void setNumberOnClock(int num){
+	  HAL_GPIO_WritePin(GPIOA, LEDPIN[num], GPIO_PIN_RESET);
+  }
+  // EXCERCISE 9
+  void clearNumberOnClock(int num){
+	  HAL_GPIO_WritePin(GPIOA, LEDPIN[num], GPIO_PIN_SET);
+  }
+
+  //EXCERCISE 10
+  int second_display = 0;
+  int minute_display = 0;
+  int hour_display = 0;
+  void second(int i){
+	  int x=i/5; // Mỗi 5s kim giây di chuyển 1 lần
+	  LED_ON(x);
+  }
+  void minute(int i){
+	  int x=i/300; // Mỗi 5p kim phút di chuyển 1 lần
+	  LED_ON(x);
+  }
+  void hour(int i){
+	  LED_ON(i);
+  }
+  void tang_counter(){
+	  TURN_OFF_ALL();
+
+	  if (second_display >= 60) second_display -=60;
+	  second(second_display);
+	  second_display++;
+
+	  if (minute_display >= 3600) {
+		  minute_display -=3600; // khi kim phút đi được 1 vòng thì kim giờ di chuyển 1 lần
+		  hour_display++;
+	  }
+	  minute(minute_display);
+	  minute_display++;
+
+	  if (hour_display >= 12) hour_display -=12;
+	  hour(hour_display);
+  }
+
+  while (1)
+  {
+
+//	  EXERCISE 6
+//	  if (counter_for_test >= 12) counter_for_test-=12;
+//	  test_LED(counter_for_test);
+//	  counter_for_test++;
+//	  HAL_Delay(1000);
+
+//    EXCERCISE 10
+	  tang_counter();
+	  HAL_Delay(1000);
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin
+                          |LED5_Pin|LED6_Pin|LED7_Pin|LED8_Pin
+                          |LED9_Pin|LED10_Pin|LED11_Pin|LED12_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : LED1_Pin LED2_Pin LED3_Pin LED4_Pin
+                           LED5_Pin LED6_Pin LED7_Pin LED8_Pin
+                           LED9_Pin LED10_Pin LED11_Pin LED12_Pin */
+  GPIO_InitStruct.Pin = LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin
+                          |LED5_Pin|LED6_Pin|LED7_Pin|LED8_Pin
+                          |LED9_Pin|LED10_Pin|LED11_Pin|LED12_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
